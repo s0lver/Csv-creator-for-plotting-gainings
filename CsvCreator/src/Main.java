32b693d3ae90ef70f10d3302a0b5c68d1aa1dd65
s0lver <rperez.github@gmail.com>
2016-05-18 17:02:47 -0500

    Addition of 90 secs files

--- CsvCreator/src/Main.java
+++ CsvCreator/src/Main.java
@@ -4,7 +4,8 @@ import java.text.ParseException;
 public class Main {
     static String[] summarizedPaths = new String[] {
             "c:\\users\\rafael\\desktop\\summarized-30.csv",
-            "c:\\users\\rafael\\desktop\\summarized-60.csv"
+            "c:\\users\\rafael\\desktop\\summarized-60.csv",
+            "c:\\users\\rafael\\desktop\\summarized-90.csv"
     };
 
     public static void main(String[] args) throws IOException, ParseException {
@@ -25,15 +26,15 @@ public class Main {
 
         String[] localFilePaths = new String[]{
                 "CsvCreator/input/records-local-30.csv",
-                "CsvCreator/input/records-local-60.csv"
+                "CsvCreator/input/records-local-60.csv",
+                "CsvCreator/input/records-local-90.csv"
         };
 
         String[] remoteFilesPaths = new String[]{
                 "CsvCreator/input/records-remote-30.csv",
-                "CsvCreator/input/records-remote-60.csv"
+                "CsvCreator/input/records-remote-60.csv",
+                "CsvCreator/input/records-remote-90.csv",
         };
-
-
 
         long init = System.currentTimeMillis();
 

4b5b1677403e4133c71b66a1ce9a970b10829ae8
s0lver <rperez.github@gmail.com>
2016-08-01 15:55:00 -0500

    Addittion of 150 seconds files

--- CsvCreator/src/Main.java
+++ CsvCreator/src/Main.java
@@ -5,13 +5,13 @@ public class Main {
     static String[] summarizedPaths = new String[] {
             "c:\\users\\rafael\\desktop\\summarized-30.csv",
             "c:\\users\\rafael\\desktop\\summarized-60.csv",
-//            "c:\\users\\rafael\\desktop\\summarized-90.csv",
-//            "c:\\users\\rafael\\desktop\\summarized-120.csv",
-//            "c:\\users\\rafael\\desktop\\summarized-150.csv",
+            "c:\\users\\rafael\\desktop\\summarized-90.csv",
+            "c:\\users\\rafael\\desktop\\summarized-120.csv",
+            "c:\\users\\rafael\\desktop\\summarized-150.csv",
     };
 
     public static void main(String[] args) throws IOException, ParseException {
-//        processIndividualExperiments();
+        processIndividualExperiments();
         processExperimentsGlobally();
     }
 
@@ -45,17 +45,17 @@ public class Main {
         String[] localFilePaths = new String[]{
                 "CsvCreator/input/r2-records-local-30.csv",
                 "CsvCreator/input/r2-records-local-60.csv",
-//                "CsvCreator/input/r2-records-local-90.csv",
-//                "CsvCreator/input/r2-records-local-120.csv",
-//                "CsvCreator/input/r2-records-local-150.csv",
+                "CsvCreator/input/r2-records-local-90.csv",
+                "CsvCreator/input/r2-records-local-120.csv",
+                "CsvCreator/input/r2-records-local-150.csv",
         };
 
         String[] remoteFilesPaths = new String[]{
                 "CsvCreator/input/r2-records-remote-30.csv",
                 "CsvCreator/input/r2-records-remote-60.csv",
-//                "CsvCreator/input/r2-records-remote-90.csv",
-//                "CsvCreator/input/r2-records-remote-120.csv",
-//                "CsvCreator/input/r2-records-remote-150.csv",
+                "CsvCreator/input/r2-records-remote-90.csv",
+                "CsvCreator/input/r2-records-remote-120.csv",
+                "CsvCreator/input/r2-records-remote-150.csv",
         };
 
         long init = System.currentTimeMillis();
